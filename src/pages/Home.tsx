3e120914c90cafe78b4b62861aa820e90a3e8e23
thananchanok <tonfern@thananchanoks-MacBook-Air.local>
2023-10-06 02:27:53 +0700

    auth context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,20 @@
 import usePosts from '../hooks/usePosts'
 import classes from './Home.module.css'
 import Post from '../components/Post'
+import { useAuth } from '../providers/AuthProvider'
 
 const Home = () => {
   const { posts, isLoading } = usePosts()
+  const { isLoggedIn } = useAuth()
 
-  if (isLoading) return <h1>Loading...</h1>
+  console.log('from home:', isLoggedIn)
+
+  if (isLoading) return <h1 style={{ textDecoration: 'none', color: ' #6666dc' }}>Loading...</h1>
 
   return (
     <div className={classes.container}>
-      <div className="feed-container">
-        <h2>Home</h2>
+      <div className={classes.feedContainer}>
+        <h2 style={{ color: 'white' }}>Home</h2>
         {posts &&
           posts.map((post) => {
             return <Post key={post.id} post={post} />
